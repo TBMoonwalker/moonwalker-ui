af7b01de7e98f359733d407511816d2ab5db0a35
TBMoonwalker <>
2025-01-03 16:27:32 +0100

    fix marker setup

--- src/components/OpenTrades.vue
+++ src/components/OpenTrades.vue
@@ -259,7 +259,7 @@ const columns_trades = (): DataTableColumns<RowData> => {
                                     }
                                     let seconds = intervals['15_minutes']
                                     
-                                    let baseorder_datetime = Math.trunc((Number(begin_timestamp) % seconds) / 1000)
+                                    let baseorder_datetime = Math.trunc(Number(begin_timestamp) / 1000) - Math.trunc((Number(begin_timestamp) % seconds) / 1000)
                                     baseorder_datetime += 60 * timezone_offset
                                     // Baseorder marker
                                     marker_data.push({
@@ -282,7 +282,7 @@ const columns_trades = (): DataTableColumns<RowData> => {
 
                                     if (rowData.safetyorder) {   
                                         rowData.safetyorder.forEach (function (val: any, i: any) {
-                                            let safetyorder_datetime = Math.trunc((Number(val.timestamp) % seconds) / 1000)
+                                            let safetyorder_datetime = Math.trunc(Number(val.timestamp) / 1000) - Math.trunc((Number(val.timestamp) % seconds) / 1000)
                                             safetyorder_datetime += 60 * Math.abs(timezone_offset)
                                             // Safetyorder marker
                                             marker_data.push({
